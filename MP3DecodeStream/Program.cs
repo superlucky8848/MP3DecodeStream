ddcbfb93e531e7f4bc09f22912913dbb7d72badc
superlucky <mail.superlucky@gmail.com>
2014-08-06 21:39:34 +0800

    测试单声道MP3

--- MP3DecodeStream/Program.cs
+++ MP3DecodeStream/Program.cs
@@ -14,39 +14,45 @@ namespace MP3DecodeStream
         {
             Console.WriteLine("Hello World!");
 
-            Mp3Stream mp3Stream = new Mp3Stream(AppDomain.CurrentDomain.BaseDirectory + @"/Sample/sample.mp3");
-
-            Console.WriteLine("Sound Format: {0}", mp3Stream.Format);
-            Console.WriteLine("Frequency: {0}", mp3Stream.Frequency);
+            Mp3Stream mp3Stream = new Mp3Stream(AppDomain.CurrentDomain.BaseDirectory + @"/Sample/sample2.mp3");
 
             WaveFile waveFile = new WaveFile();
-            int openfileResult = waveFile.OpenFile(AppDomain.CurrentDomain.BaseDirectory + @"Sample/sample.wav", WaveFormat.WF_PCM_S16LE, false, 44100, 1);
+            int openfileResult = waveFile.OpenFile(AppDomain.CurrentDomain.BaseDirectory + @"/Sample/sample2.wav", WaveFormat.WF_PCM_S16LE, false, 8000, 1);
             if (openfileResult == 0)
             {
                 short[,] data = null;
                 byte[] buffer = new byte[512];
-                waveFile.CreateData(ref data, 128);
-                Console.WriteLine("Data Length: [{0},{1}]", data.GetLength(0), data.GetLength(1));
+                int ret;
 
-                int ret = -1;
-                double p1, p2;
-                while (ret != 0)
+                ret = mp3Stream.Read(buffer, 0, buffer.Length);
+                Console.WriteLine("File Format: {0}", mp3Stream.Format);
+                Console.WriteLine("Frequency: {0}", mp3Stream.Frequency);
+                Console.WriteLine("Channel Count: {0}", mp3Stream.ChannelCount);
+                
+                if (mp3Stream.ChannelCount <= 0 || mp3Stream.Frequency <= 0)
                 {
-                    ret = mp3Stream.Read(buffer, 0, buffer.Length);
-                    int i;
-                    for (i = 0; i < data.GetLength(1); i++)
+                    Console.WriteLine("Cannnot decode file.");
+                }
+                else
+                {
+                    waveFile.CreateData(ref data, 128);
+                    Console.WriteLine("Data Length [{0},{1}]", data.GetLength(0), data.GetLength(1));
+
+                    while (ret != 0)
                     {
-                        if (i * 4 >= ret) break;
-                        //Single channel with sound mix.
-                        p1 = (double)BitConverter.ToInt16(buffer, i * 4) / 32767.0;
-                        p2 = (double)BitConverter.ToInt16(buffer, i * 4 + 2) / 32767.0;
-                        //data[0, i] = (short)((p1 + p2 - p1 * p2) * 32767.0);
-                        data[0, i] = (short)((p1 + p2) * 32767.0 / 2.0);
-                        //Stero channel
-                        //data[0, i] = BitConverter.ToInt16(buffer, i * 4);
-                        //data[1, i] = BitConverter.ToInt16(buffer, i * 4 + 2);
+                        int i;
+                        for (i = 0; i < data.GetLength(1); i++)
+                        {
+                            if (i * 4>= ret) break;
+                            double p1 = (double)BitConverter.ToInt16(buffer, i * 2) / 32767.0;
+                            data[0, i] = (short)(
+                                                    (     (double)BitConverter.ToInt16(buffer, i * 4) / 32767.0
+                                                        + (double)BitConverter.ToInt16(buffer, i * 4 + 2) / 32767.0
+                                                    ) * 32767.0 / 2.0);
+                        }
+                        waveFile.PutData(data, i);
+                        ret = mp3Stream.Read(buffer, 0, buffer.Length);
                     }
-                    waveFile.PutData(data, i);
                 }
                 waveFile.FlushFile();
                 waveFile.CloseFile();
